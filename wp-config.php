53caa8088f3636b6c82d25481fa9686484983f1d
shioringo <shioshioly@gmail.com>
2020-05-02 15:50:03 +0900

    wp-configにDB接続情報を書き込み

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,47 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress01' );
+// Develop（localhost）
+if (@$_SERVER["SERVER_NAME"] === 'localhost') {
+	//ローカルホスト
+	define( 'DB_NAME', 'wordpress01' );
 
-/** MySQL database username */
-define( 'DB_USER', 'root' );
+	/** MySQL database username */
+	define( 'DB_USER', 'root' );
 
-/** MySQL database password */
-define( 'DB_PASSWORD', '1234' );
+	/** MySQL database password */
+	define( 'DB_PASSWORD', '1234' );
 
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+	/** MySQL hostname */
+	define( 'DB_HOST', 'localhost' );
 
-/** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+	/** Database Charset to use in creating database tables. */
+	define( 'DB_CHARSET', 'utf8mb4' );
 
-/** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+	/** The Database Collate type. Don't change this if in doubt. */
+	define( 'DB_COLLATE', '' );
+
+} else {
+	//heroku本番環境
+	$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+	/** The name of the database for WordPress */
+	define('DB_NAME', trim($url['path'], '/'));
+
+	/** MySQL database username */
+	define('DB_USER', $url['user']);
+
+	/** MySQL database password */
+	define('DB_PASSWORD', $url['pass']);
+
+	/** MySQL hostname */
+	define('DB_HOST', $url['host']);
+
+	/** Database Charset to use in creating database tables. */
+	define('DB_CHARSET', 'utf8');
+
+	/** The Database Collate type. Don't change this if in doubt. */
+	define('DB_COLLATE', '');
+}
 
 /**#@+
  * Authentication Unique Keys and Salts.
